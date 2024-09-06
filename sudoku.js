c7027cba7fe94bc6c7b3a606852cb1a5fe4a07e7
uhubar <uhubar.int@gmail.com>
2024-09-06 22:39:30 +0300

    Solve function taken puzzle strings & return answer for sudoku massiv

--- sudoku.js
+++ sudoku.js
@@ -16,45 +16,46 @@ function read() {
   }
   return allVariants[0];
 }
+const getArrPuzzle = read();
 
-function solve() {
+function solve(getArrPuzzle, row, col, k) {
   for (let i = 0; i < 9; i++) {
-    const m = 3 * Math.floor(row / 3) + Math.floor(i / 3);
-    const n = 3 * Math.floor(col / 3) + i % 3;
-    if (board[row][i] == k || board[i][col] == k || board[m][n] == k) {
-      return false;
-    }
-}
-return true;
-}
-
-function isSolved(board, row, col, k) {
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      if (data[i][j] == '-') {
-        for (let k = 1; k <= 9; k++) {
-          if (solve(data, i, j, k)) {
-            data[i][j] = `${k}`;
-          if (isSolved(data)) {
-           return true;
-          } else {
-           data[i][j] = '-';
-          }
-         }
-       }
-       return false;
-     }
-   }
+      const m = 3 * Math.floor(row / 3) + Math.floor(i / 3);
+      const n = 3 * Math.floor(col / 3) + i % 3;
+      if (getArrPuzzle[row][i] == k || getArrPuzzle[i][col] == k || getArrPuzzle[m][n] == k) {
+        return false;
+      }
   }
   return true;
 }
 
-function prettyBoard(arr) {
-  console.log(" ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★");
-  arr.forEach((el) => console.log(" | " + el.join(" | ") + " | "));
-  console.log(" ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★");
+function isSolved(getArrPuzzle) {
+for (let i = 0; i < 9; i++) {
+  for (let j = 0; j < 9; j++) {
+    if (getArrPuzzle[i][j] == '-') {
+      for (let k = 1; k <= 9; k++) {
+        if (solve(getArrPuzzle, i, j, k)) {
+          getArrPuzzle[i][j] = `${k}`;
+        if (isSolved(getArrPuzzle)) {
+         return true;
+        } else {
+          getArrPuzzle[i][j] = '-';
+        }
+       }
+     }
+     return false;
+   }
+ }
 }
+return true;
+}
+isSolved(getArrPuzzle);
+console.log(getArrPuzzle);
 
-isSolved(read());
-console.log(read());
-console.log(prettyBoard(board));
+function prettyBoard(arr) {
+  console.table(" -------------------------------------");
+  arr.forEach((el) => console.table(" | " + el.join(" | ") + " | "));
+  return(" -------------------------------------");
+}
+console.table(prettyBoard(getArrPuzzle));
+
