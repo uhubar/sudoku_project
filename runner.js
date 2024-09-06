173502e52dda66bd6508a9be8352310b7e059920
Dima <klimmerst@gmail.com>
2024-09-06 16:21:12 +0300

    build: runner feature

--- runner.js
+++ runner.js
@@ -1,1 +1,17 @@
-// Используйте для решения судоку необходимые функции из файла sudoku.js
+const { read, prettyBoard } = require('./sudoku');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question('Выберите вариант игры: от 1 до 15 --> ', (gameNumber) => {
+  const games = read();
+
+  if (gameNumber > 5) {
+    console.log('Ух! А ты рисковый');
+  } else if (gameNumber === 15) {
+    console.log('Не советую даже начинать');
+  }
+  prettyBoard(games[gameNumber - 1]);
+  readline.close();
+});
